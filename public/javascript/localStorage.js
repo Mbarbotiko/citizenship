4ad96f63fd557899cc7d600b676a547e24ab9a9a
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-01-21 09:48:13 -0600

    file

---
+++ public/javascript/localStorage.js
@@ -0,0 +1,9 @@
+//select input user-input main
+
+//use regex to search for email characters
+
+//start test button should be unclickable until regex search has been satisfied
+
+//on submit save the users email to local storage
+
+//in place of the input field if it is a returning user remove the input field and show their email address with a hello message
